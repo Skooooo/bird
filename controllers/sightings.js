2f4346eaabcf8c7f7720173f895fa1fd53cb93a2
AmirModibbo <128089748+AmirModibbo@users.noreply.github.com>
2023-05-19 16:45:01 +0100

    final branch

--- controllers/sightings.js
+++ controllers/sightings.js
@@ -2,9 +2,12 @@
 var bodyParser = require("body-parser");  // Parses JSON bodies for requests
 var req = require('request');  // Library for making HTTP requests
 var Sighting = require('../models/sightings');  // Import Sighting model
-var path = require('path');  // Helps manipulate file paths
+var path = require('path');
+const fetch = require("node-fetch");  // Helps manipulate file paths
+const SPARQL_URL = 'https://dbpedia.org/sparql';
 
-// Define an async function to create a new sighting
+
+//Define an async function to create a new sighting
 exports.create = async function (req, res) {
     // Get user data from the request body
     userData = req.body;
@@ -39,8 +42,140 @@ exports.create = async function (req, res) {
         // If there's an error, send a 500 status and the error message
         res.status(500).send(`Invalid data! Error: ${err.message}`);
 
+    }
+};
+
+    exports.getSightings = async function (req, res) {
+        try {
+            // Fetch all sightings from the database
+            const sightings = await Sighting.find({});
+            // Render the sightings_list view with the fetched data
+            res.render("bird_list", {title: "", sightings});
+        } catch (error) {
+            // print error message in the console
+            console.error(error);
+            // rendering error page with promopt
+            res.status(500).send("Error retrieving sightings from the database.");
+        }
     };
 
+    exports.getRecentSightings = async function (req, res) {
+        try {
+            // Fetch all sightings from the database, sorted by dateTimeSeen
+            const sightings = await Sighting.find({}).sort({dateTimeSeen: -1});
 
+            // Render the bird_recent view with the fetched data
+            res.render("bird_recent", {title: "", sightings});
+        } catch (error) {
+            // print error message in the console
+            console.error(error);
+            // rendering error page with promopt
+            res.status(500).send("Error retrieving sightings from the database.");
+        }
+    };
 
-};
+    exports.getSightingDetails = async function (req, res, next) {
+        try {
+
+            // retrieving sightingId from request params
+            const sightingId = req.params.id;
+            // searching entry from the database by id
+            const sighting = await Sighting.findById(sightingId);
+            if (sighting) {
+                // transform bird speci name to html encode name
+                const birdName = encodeURIComponent(sighting.identification);
+                // dbpedia query
+                const query = `
+        PREFIX dbo: <http://dbpedia.org/ontology/>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        SELECT ?label ?description ?uri WHERE {
+            ?uri a dbo:Bird .
+            ?uri dbo:abstract ?description ;
+                rdfs:label ?label .
+            ?uri foaf:isPrimaryTopicOf ?wikiPage .
+            FILTER (LANG(?label) = 'en' && LANG(?description) = 'en' && STRSTARTS(LCASE(?label), "${birdName.toLowerCase()}"))
+           }
+           LIMIT 1
+            `;
+                // retrieving data ftom dbpedia url as json format
+                const url = `${SPARQL_URL}?query=${encodeURIComponent(
+                    query
+                )}&format=json`;
+                // retrieving data from dbpedia
+                const response = await fetch(url);
+                // retrieving data from response
+                const data = await response.json();
+                // determine if it has the entry
+                if (data.results.bindings.length > 0) {
+                    // retrieving from bindings index 0
+                    const birdInfo = data.results.bindings[0];
+                    // creating an object base on the information from dbpedia
+                    sighting.birdInfo = {
+                        name: birdInfo.label.value,
+                        description: birdInfo.description.value,
+                        uri: birdInfo.uri.value,
+                    };
+                }
+                // render page baseon the object just created
+                res.render("bird_details", {title: "", sighting: sighting});
+            } else {
+                // if the entry does not exist in the database
+                // render the page of 404 with prompt
+                res.status(404).send("Sighting not found");
+            }
+        } catch (err) {
+            // if error detected, render 500 page with prompt
+            res.status(500).send(`Error: ${err.message}`);
+        }
+    };
+
+    exports.showUpdateForm = async function (req, res, next) {
+        try {
+            // retrieving sightingId from request params
+            const sightingId = req.params.id;
+            // searching entry from the database by id
+            const sighting = await Sighting.findById(sightingId);
+            // if the sighting entry exists
+            if (sighting) {
+                // render the page of update with the entry that just found
+                res.render("update", {title: "Update a sighting", sighting: sighting});
+            } else {
+                // if the entry does not exist in the database
+                // render the page of 404 with prompt
+                res.status(404).send("Sighting not found");
+            }
+        } catch (err) {
+            // if error detected, render 500 page with prompt
+            res.status(500).send(`Error: ${err.message}`);
+        }
+    };
+
+    exports.updateSighting = async function (req, res) {
+        try {
+            const sightingId = req.params.id;
+            const newIdentification = req.body.identification;
+            const posterId = req.body.posterId;
+
+            // Fetch the sighting from the database
+            const sighting = await Sighting.findById(sightingId);
+
+            if (sighting) {
+                // Check if the ID matches the original poster's ID
+                if (sighting.posterId === posterId) {
+                    // Update the sighting
+                    sighting.identification = newIdentification;
+                    await sighting.save();
+
+                    res.redirect(`/sighting/${sightingId}`);
+                } else {
+                    res.status(403).send('Unauthorized to update this sighting');
+                }
+            } else {
+                res.status(404).send('Sighting not found');
+            }
+        } catch (err) {
+            res.status(500).send(`Error: ${err.message}`);
+        }
+    };
+
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ const fetch = require('node-fetch');
 const SPARQL_URL = 'https://dbpedia.org/sparql';
 const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Types;
+var controllers = require('../controllers/sightings');
+
 
 // Configure multer storage options
 var storage = multer.diskStorage({
@@ -30,19 +32,8 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage });
 
 // Bird list route
-router.get("/", async function (req, res) {
-  try {
-    // Fetch all sightings from the database
-    const sightings = await Sighting.find({});
-    // Render the sightings_list view with the fetched data
-    res.render("bird_list", { title: "", sightings });
-  } catch (error) {
-    // print error message in the console
-    console.error(error);
-    // rendering error page with promopt
-    res.status(500).send("Error retrieving sightings from the database.");
-  }
-});
+router.get('/', controllers.getSightings);
+
 
 router.get('/nearby', async function (req, res) {
   try {
@@ -59,21 +50,7 @@ router.get('/nearby', async function (req, res) {
 
 // Recent bird route
 
-router.get('/recent', async function (req, res) {
-
-  try {
-    // Fetch all sightings from the database, sorted by dateTimeSeen
-    const sightings = await Sighting.find({}).sort({ dateTimeSeen: -1 });
-
-    // Render the bird_recent view with the fetched data
-    res.render("bird_recent", { title: "", sightings });
-  } catch (error) {
-    // print error message in the console
-    console.error(error);
-    // rendering error page with promopt
-    res.status(500).send("Error retrieving sightings from the database.");
-  }
-});
+router.get('/recent', controllers.getRecentSightings);
 
 router.get("/add_bird", function (req, res) {
   res.render("add_bird", { title: "Enter a sighting information" });
@@ -110,25 +87,21 @@ const imgParse = (file) => {
 // Define a route for POST requests to '/add'
 router.post("/add", upload.single("myImg"), async function (req, res) {
   // Log the request body to the console
-  console.log("Request body:", req.body);
+  console.log('Request body:', req.body);
 
   try {
-    // retrieving values from req.body
-    const { identification, description, dateTimeSeen, nickname, location } =
-        req.body;
     // Create a new sighting object from the request body
     const newSighting = new Sighting({
-      identification,
-      description,
+      identification: req.body.identification,
+      description: req.body.description,
       // Store the image path, removing the 'public' part of the path
-      // Here is the image saved in base64 format
-      img: imgParse(img),
-      dateTimeSeen,
-      nickname,
+      img: req.file.path.replace('public', ''),
+      dateTimeSeen: req.body.dateTimeSeen,
+      nickname: req.body.nickname,
       location: {
-        type: "Point",
+        type: 'Point',
         // Get the coordinates from the request body
-        coordinates: location.coordinates,
+        coordinates: req.body.location.coordinates
       },
       // Set the current date and time
       datetime: new Date(),
@@ -137,126 +110,26 @@ router.post("/add", upload.single("myImg"), async function (req, res) {
     // Save the new sighting to the database
     await newSighting.save();
     // Send a success message as the response
-
-    res.json({ message: 'Success' });
-
+    res.json({message: 'Success'});
   } catch (error) {
     // Log any errors to the console
     console.error(error);
     // Send an error message as the response, with a status code of 500
-    res.status(500).send("Error saving sighting to the database.");
+    res.status(500).send('Error saving sighting to the database.');
   }
 });
 
 // Bird details/dbpedia
-router.get("/sighting/:id", async function (req, res, next) {
-  try {
-
-    // retrieving sightingId from request params
-    const sightingId = req.params.id;
-    // searching entry from the database by id
-    const sighting = await Sighting.findById(sightingId);
-    if (sighting) {
-      // transform bird speci name to html encode name
-      const birdName = encodeURIComponent(sighting.identification);
-      // dbpedia query
-      const query = `
-        PREFIX dbo: <http://dbpedia.org/ontology/>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-        SELECT ?label ?description ?uri WHERE {
-            ?uri a dbo:Bird .
-            ?uri dbo:abstract ?description ;
-                rdfs:label ?label .
-            ?uri foaf:isPrimaryTopicOf ?wikiPage .
-            FILTER (LANG(?label) = 'en' && LANG(?description) = 'en' && STRSTARTS(LCASE(?label), "${birdName.toLowerCase()}"))
-           }
-           LIMIT 1
-            `;
-      // retrieving data ftom dbpedia url as json format
-      const url = `${SPARQL_URL}?query=${encodeURIComponent(
-          query
-      )}&format=json`;
-      // retrieving data from dbpedia
-      const response = await fetch(url);
-      // retrieving data from response
-      const data = await response.json();
-      // determine if it has the entry
-      if (data.results.bindings.length > 0) {
-        // retrieving from bindings index 0
-        const birdInfo = data.results.bindings[0];
-        // creating an object base on the information from dbpedia
-        sighting.birdInfo = {
-          name: birdInfo.label.value,
-          description: birdInfo.description.value,
-          uri: birdInfo.uri.value,
-        };
-      }
-      // render page baseon the object just created
-      res.render("bird_details", { title: "", sighting: sighting });
-    } else {
-      // if the entry does not exist in the database
-      // render the page of 404 with prompt
-      res.status(404).send("Sighting not found");
-    }
-  } catch (err) {
-    // if error detected, render 500 page with prompt
-    res.status(500).send(`Error: ${err.message}`);
-  }
-});
+router.get('/sighting/:id', controllers.getSightingDetails);
 
 
 //update identification
-router.get("/sighting/:id/update", async function (req, res, next) {
-  try {
-    // retrieving sightingId from request params
-    const sightingId = req.params.id;
-    // searching entry from the database by id
-    const sighting = await Sighting.findById(sightingId);
-    // if the sighting entry exists
-    if (sighting) {
-      // render the page of update with the entry that just found
-      res.render("update", { title: "Update a sighting", sighting: sighting });
-    } else {
-      // if the entry does not exist in the database
-      // render the page of 404 with prompt
-      res.status(404).send("Sighting not found");
-    }
-  } catch (err) {
-    // if error detected, render 500 page with prompt
-    res.status(500).send(`Error: ${err.message}`);
-  }
-});
+router.get('/sighting/:id/update', controllers.showUpdateForm);
+
+
 
 //post identification
-router.post("/sighting/:id/update", async function (req, res) {
-  try {
-    const sightingId = req.params.id;
-    const newIdentification = req.body.identification;
-    const posterId = req.body.posterId;
-
-    // Fetch the sighting from the database
-    const sighting = await Sighting.findById(sightingId);
-
-    if (sighting) {
-      // Check if the ID matches the original poster's ID
-      if (sighting.posterId === posterId) {
-        // Update the sighting
-        sighting.identification = newIdentification;
-        await sighting.save();
-
-        res.redirect(`/sighting/${sightingId}`);
-      } else {
-        res.status(403).send('Unauthorized to update this sighting');
-      }
-    } else {
-      res.status(404).send('Sighting not found');
-    }
-  } catch (err) {
-    res.status(500).send(`Error: ${err.message}`);
-  }
-
-});
+router.post('/sighting/:id/update', controllers.updateSighting);
 
 // // Parse JSON bodies
 // router.use(bodyParser.json());
